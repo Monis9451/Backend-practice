fa80aebf9801a12a945d2b993b6801ff2f628a07
Monis9451 <mianmonishussain786@gmail.com>
2024-07-23 13:49:21 +0500

    Use process.argv function

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let i = 5
-for(i=0; i<5; i++){
-    console.log("Hello")
-}
+// let i = 5
+// for(i=0; i<5; i++){
+//     console.log("Hello")
+// }
 
 //Countinuing my coding streak
+
+let totalArgs = process.argv
+for (i=0; i<totalArgs.length; i++){
+    console.log(totalArgs[i])
+}
+
+// console.log(...process.argv)
+
+// let obj = [
+//     names = "Monis",
+//     age = 18
+// ]
+
+// console.log(obj.age)
