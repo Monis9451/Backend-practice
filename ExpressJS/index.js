0f83db65fbb51272eacf0dd3e08a9830a731eda9
Monis9451 <mianmonishussain786@gmail.com>
2024-07-28 11:16:07 +0500

    Practice express's listen and get keywords

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,1 +1,23 @@
 const express = require("express")
+const app = express()
+// console.dir(app)
+let port = 3000
+
+app.listen(port,()=>{
+    console.log(`App is listening on port ${port}`)
+})
+
+app.get("/",(req,res)=>{
+    console.log("You requested root path")
+})
+app.get("/apple",(req,res)=>{
+    console.log("You requested apple path")
+})
+app.get("/orange",(req,res)=>{
+    console.log("You requested orange path")
+})
+
+// app.use((req,res)=>{
+//     console.log("Your request received")
+//     res.send("This is a basic response")
+// })
